2f1e9b44b033077904ae6023fd65413178a7e7f5
fawwad93-source <fawwad93@gmail.com>
2026-09-11 01:01:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nms-dashboard-v5';
+const CACHE = 'nms-dashboard-v7';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
