d97a04565769e93a9010de6b70fac73d1d40dde8
fawwad93-source <fawwad93@gmail.com>
2026-09-24 00:35:58 +0800

    Cash Recon v18 - audit period and manual full-day sales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nms-dashboard-v17';
+const CACHE = 'nms-dashboard-v18';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
